0a271c1e5217baef1abe52fb03b88fffc5ce9fc3
Naveen <64878189+mialeevs@users.noreply.github.com>
2024-09-14 16:42:17 -0400

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -150,6 +150,8 @@ rm argocd-linux-amd64
 # Get the password from the secret file
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
+sudo su -c "chown -R ubuntu:ubuntu /mnt" ubuntu
+
 git clone -b  lab https://github.com/mialeevs/devops_tools_aws_k8s.git
 
 cd devops_tools_aws_k8s/config/stack
