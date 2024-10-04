69559f307c54f8c83225fc0de77437b95d414141
Naveen <64878189+mialeevs@users.noreply.github.com>
2024-10-04 08:23:03 -0400

    Updated k8s version

--- tools/install.sh
+++ tools/install.sh
@@ -8,34 +8,33 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software
 echo "Exporting OS and CRIO Version"
 
 export OS_VERSION=xUbuntu_22.04
-export CRIO_VERSION=1.30
+export CRIO_VERSION=v1.30
+export KUBERNETES_VERSION=v1.30
 
-sudo curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS_VERSION/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-archive-keyring.gpg
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
-sudo curl -fsSL https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS_VERSION/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/libcontainers-crio-archive-keyring.gpg
+sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 
-# we can get the latest release details from https://cri-o.io/
+sudo echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
 
-sudo echo "deb [signed-by=/usr/share/keyrings/libcontainers-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS_VERSION/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 
-sudo echo "deb [signed-by=/usr/share/keyrings/libcontainers-crio-archive-keyring.gpg] https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS_VERSION/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
+sudo apt-get update -y
+sudo apt-get install cri-o -y
 
-echo "Exporting kubernetes repositories and keyrings values"
-
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "Updating the repositories"
 # Update the repositiries
 sudo apt-get update -y
 sudo apt-get install cron -y
 
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 echo "Installing cri-o and kubernetes tools"
 
 # Use the same versions to avoid issues with the installation.
-sudo apt-get install -y cri-o cri-tools kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl
 
 sleep 5
 
